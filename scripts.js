cc15562856ff993de17e5e48e653bf036c2197d0
Waeting <waeting.c@gmail.com>
2023-05-06 00:25:41 +0800

    調整樣式，新增第二篇，上傳照片

--- scripts.js
+++ scripts.js
@@ -1,12 +1,38 @@
-var likeBtn = document.querySelector('.like-btn');
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper1 = new Swiper('.swiper-container-1', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination-1',
+            clickable: true,
+        },
+    });
 
-likeBtn.addEventListener('click', function () {
-    var plusOne = document.createElement('span');
-    plusOne.classList.add('plus-one');
-    plusOne.textContent = '+1';
-    likeBtn.appendChild(plusOne);
+    var swiper2 = new Swiper('.swiper-container-2', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination-2',
+            clickable: true,
+        },
+    });
 
-    setTimeout(function () {
-        likeBtn.removeChild(plusOne);
-    }, 1000);
+    var likeBtns = document.querySelectorAll('.like-btn');
+
+    likeBtns.forEach(function (likeBtn) {
+        likeBtn.addEventListener('click', function () {
+            var plusOne = document.createElement('span');
+            plusOne.classList.add('plus-one');
+            plusOne.textContent = '+1';
+            likeBtn.appendChild(plusOne);
+
+            setTimeout(function () {
+                likeBtn.removeChild(plusOne);
+            }, 1000);
+        });
+    });
 });
